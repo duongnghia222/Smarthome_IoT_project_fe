3880b00a9a2566916051ae06b43c6257a5b65ee5
git <duongnghia222>
2023-05-06 01:30:44 +0700

    add ai

---
+++ Face_Recognition/recognize_faces_video.py
@@ -0,0 +1,68 @@
+from os import write
+import face_recognition
+import pickle
+import cv2
+import time
+import imutils
+
+detection_method = 'hog'
+encodings_file = 'encodings.pickle'
+
+with open(encodings_file, "rb") as f:
+    encoding_data = f.read()
+data = pickle.loads(encoding_data)
+
+print("Starting video stream...")
+video = cv2.VideoCapture(0)
+time.sleep(2.)
+
+while True:
+    ret, frame = video.read()
+
+    if not ret:
+        break
+
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    rgb = imutils.resize(rgb, width=750)
+    r = frame.shape[1] / float(rgb.shape[1])
+    boxes = face_recognition.face_locations(rgb, model=detection_method)
+    encodings = face_recognition.face_encodings(rgb, boxes)
+    names = []
+
+    for encoding in encodings:
+        matches = face_recognition.compare_faces(data["encodings"], encoding)
+        name = "Unknown"
+        if True in matches:
+            matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+            counts = {}
+            for i in matchedIdxs:
+                name = data["names"][i]
+                counts[name] = counts.get(name, 0) + 1
+
+            name = max(counts, key=counts.get)
+
+        names.append(name)
+
+    for ((top, right, bottom, left), name) in zip(boxes, names):
+        top = int(top * r)
+        right = int(right * r)
+        bottom = int(bottom * r)
+        left = int(left * r)
+
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+        y = top - 15 if top - 15 > 15 else top + 15
+
+        cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1)
+
+    cv2.imshow("Image", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+video.release()
+cv2.destroyAllWindows() 
+
+
+
+
+
+
